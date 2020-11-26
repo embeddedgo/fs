6d494707ba8e67897cdc0bd5e5d6df7ff6ad9ca4
Michał Derkacz <michal@Lnet.pl>
2020-11-26 19:48:46 +0100

    termfs: fix FS.Open method

--- termfs/termfs.go
+++ termfs/termfs.go
@@ -50,9 +50,11 @@ func (fsys *FS) OpenWithFinalizer(name string, flag int, perm fs.FileMode, close
 	return &file{fsys, flag, closed}, nil
 }
 
+func nop() {}
+
 // Open implements the fs.FS Open method.
 func (fsys *FS) Open(name string) (fs.File, error) {
-	return fsys.OpenWithFinalizer(name, syscall.O_RDONLY, 0, nil)
+	return fsys.OpenWithFinalizer(name, syscall.O_RDONLY, 0, nop)
 }
 
 // Type implements the rtos.FS Type method
