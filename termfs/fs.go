d76f2b0f934cb0f7c771c33e36ae062b9a35f7db
Michał Derkacz <michal@Lnet.pl>
2021-02-22 00:04:03 +0100

    termfs: fix package docstring

--- termfs/fs.go
+++ termfs/fs.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package termfs provides a lightwait implementation of terminal device
+// Package termfs provides a lightweight implementation of terminal device
 // filesystem.
 package termfs
 
--- termfs/lightfs.go
+++ termfs/lightfs.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package termfs provides a lightwait implementation of terminal device
-// filesystem.
 package termfs
 
 import (
